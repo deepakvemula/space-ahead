ce89861260553a1920f01a8e00a1c691a7411ad6
Siddhesh Thadeshwar <siddhesht@outlook.in>
2024-02-23 16:46:04 +0530

    feat(app): Major UI refactor & new additions
    Added RSS feed, Added theme toggle, Added Preact component; Optimized CSS; Added Mobile Nav; New Tags pages; and more...
    
    Closes #2

--- src/components/SpaceEmoji.jsx
+++ src/components/SpaceEmoji.jsx
@@ -4,5 +4,5 @@ export default function SpaceEmoji({emojis}) {
   const getRandomEmoji = () => emojis[(Math.floor(Math.random() * emojis.length))];
   const [emoji, setEmoji] = useState(getRandomEmoji());
 
-  return <button class="kanit-regular text-balance" title="Click Me!" style={{fontSize: "1rem", background: "transparent", border: "none", cursor: "pointer", color: "whitesmoke", marginBottom: 0}} onClick={() => setEmoji(getRandomEmoji())}>{emoji}</button>;
+  return <button id="space-pop-culture" class="kanit-regular text-balance" title="Click Me!" style={{fontSize: "1rem", background: "transparent", border: "none", cursor: "pointer", color: "inherit", marginBottom: 0, textOverflow: "wrap"}} onClick={() => setEmoji(getRandomEmoji())}>{emoji}</button>;
 }
